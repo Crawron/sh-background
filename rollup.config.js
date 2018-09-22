0704178516319bebc1b895935263a2ae7b49cd49
Crawron <azertyaaron@gmail.com>
2018-09-22 14:20:35 -0500

    importing ccapture with rollup

--- rollup.config.js
+++ rollup.config.js
@@ -4,21 +4,29 @@ import resolve from "rollup-plugin-node-resolve"
 import progress from "rollup-plugin-progress"
 import serve from "rollup-plugin-serve"
 import html from "rollup-plugin-generate-html-template"
+import copy from "rollup-plugin-copy"
 
 export default {
 	input: "./src/main.ts",
 	output: {
 		file: "./dist/main.js",
 		format: "iife",
-		sourcemap: true
+		sourcemap: true,
+		globals: {
+			"ccapture.js": "CCapture"
+		}
 	},
+	external: ["ccapture.js"],
 	plugins: [
 		progress(), //
-		resolve({ three: true }),
+		resolve(),
 		commonjs(),
 		typescript({
 			clean: true,
 			rollupCommonJSResolveHack: true
+		}),
+		copy({
+			"node_modules/ccapture.js/build/CCapture.all.min.js": "dist/CCapture.all.min.js"
 		}),
 		html({
 			template: "src/template.html",
